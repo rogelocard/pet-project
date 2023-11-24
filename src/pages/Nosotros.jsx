19299aee0ae61f98b2cc67541fc6a318a9cecb04
Jey2503KM <102761750+Jey2503KM@users.noreply.github.com>
2023-11-23 19:39:21 -0500

    nosotros

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import styles from '../style/nosotros.module.css';
+import img1 from '../assets/img/nosotros1.jpg';
+import img2 from '../assets/img/nosotros2.jpg';
 const Nosotros = () => {
   return (
     <>
-      
+      <h1 className='text-center'>Sobre nosotros</h1>
+      <div className={styles.contenidoNosotros}>
+        <img src={img1} alt="Servicio1" />
+        <p>Buena medicina más un excelente servicio para ti y tu mascota son nuestras premisas de una buena atención a nuestros clientes.</p>
+        <p>Pet-Master es un nuevo concepto en atención médica veterinaria. Nuestra misión es hacer que tú y tu mascota tengan una relación: feliz, responsable y duradera.</p>
+        <img src={img2} alt="Servicio1" />
+      </div>
+
     </>
-    
+
   )
 }
 
